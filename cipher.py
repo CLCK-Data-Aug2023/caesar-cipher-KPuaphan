ec81c060201e15273fa9412b13921c6b6b88ae67
Kevin Puaphan <kevin.puaphan@gmail.com>
2023-11-12 23:40:32 -0500

    New Caesar Cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,15 @@
-# add your code here
+import string
+
+def caesar(text, shift, alphabets):
+
+    def shift_alphabet(alphabet):
+        return alphabet[shift:] + alphabet [:shift]
+    
+    shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+    final_alphabet = ''.join(alphabets)
+    final_shifted_alphabet = ''.join(shifted_alphabets)
+    table = str.maketrans(final_alphabet, final_shifted_alphabet)
+    return text.translate(table)
+
+text = input("Enter a random sentence: ")
+print(caesar(text, 5, [string.ascii_lowercase, string.ascii_uppercase]))
